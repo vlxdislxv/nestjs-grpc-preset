2e583079e4bd017af04e7e992199c2a3f19ae101
Vladislav <skyvvader@gmail.com>
2020-12-07 10:39:21 +0200

    setup fastify

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,10 @@
 import { ClassSerializerInterceptor } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new FastifyAdapter());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   await app.listen(3000);
 }
